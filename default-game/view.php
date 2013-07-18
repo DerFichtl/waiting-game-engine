9797a183335e924686d61494f7cb66eec5bc3266
Michael Feichtinger <derfichtl@gmail.com>
2013-07-18 21:56:48 +0200

    Analytics code ...

--- default-game/view.php
+++ default-game/view.php
@@ -8,6 +8,17 @@
 		<link rel="stylesheet" href="default-game/game.css">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
 		<script src="game.js"></script>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-682459-4']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
 	</head>
 	<body>
 
